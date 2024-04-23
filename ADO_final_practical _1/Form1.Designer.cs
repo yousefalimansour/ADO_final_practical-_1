043cca346627c37a09c3841a75347317e24a0628
Yousef Ali <you@example.com>
2024-04-23 18:27:45 +0200

    Add project files.

---
+++ ADO_final_practical _1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ADO_final_practical__1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdview = new System.Windows.Forms.DataGridView();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grdview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdview
+            // 
+            this.grdview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdview.Location = new System.Drawing.Point(0, 0);
+            this.grdview.Name = "grdview";
+            this.grdview.Size = new System.Drawing.Size(799, 381);
+            this.grdview.TabIndex = 0;
+            // 
+            // btnload
+            // 
+            this.btnload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnload.Location = new System.Drawing.Point(12, 399);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(75, 23);
+            this.btnload.TabIndex = 1;
+            this.btnload.Text = "Load";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.btnload_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnsave.Location = new System.Drawing.Point(694, 399);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 2;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.grdview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdview;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
+
---
+++ ADO_final_practical _1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_final_practical__1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            sqlcmdselect = new SqlCommand("select * from Product",sqlcn);
+            sqlcmdselectsupplier = new SqlCommand("select id as SID ,CompanyName from Supplier", sqlcn);
+
+            DA=new SqlDataAdapter(sqlcmdselect);
+            supplier=new SqlDataAdapter(sqlcmdselectsupplier);
+
+            dt=new DataTable();
+            dtsupplier = new DataTable();
+        }
+        SqlConnection sqlcn=new SqlConnection("Data Source=.;Initial Catalog=Northwind2021;Integrated Security=True");
+
+        SqlCommand sqlcmdselect;
+        SqlCommand sqlcmdselectsupplier;
+
+        SqlDataAdapter DA;
+        SqlDataAdapter supplier;
+
+        DataTable dt;
+        DataTable dtsupplier;
+     
+       
+
+        BindingSource prdbindingsource;
+        private void btnload_Click(object sender, EventArgs e)
+        {
+
+            DA.Fill(dt);     
+            prdbindingsource = new BindingSource(dt,"");
+            grdview.DataSource = prdbindingsource;
+            grdview.Columns["id"].ReadOnly = true;
+            //grdview.Columns["SupplierId"].Visible = false;
+            ///////
+            ///////
+            DataGridViewComboBoxColumn dc = new DataGridViewComboBoxColumn();
+            dc.HeaderText = "Supplier";
+            grdview.Columns.AddRange(dc);
+            supplier.Fill(dtsupplier);
+            dc.DataSource = dtsupplier;
+            dc.DisplayMember = "CompanyName";
+            dc.ValueMember = "SID";
+            dc.DataPropertyName = "id";
+          
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            grdview.EndEdit();
+            //DA.Update(dt);
+            foreach (DataRow dr in dtsupplier.Rows)
+            {
+                Debug.WriteLine(dr.RowState);
+            }
+        }
+    }
+}
